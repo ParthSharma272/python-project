378ec148c1cf510f837981e4fc200f79e748296a
ParthSharma272 <parthsharma23212@gmail.com>
2025-05-23 12:39:44 +0530

    commit 7

--- Hangman.py
+++ Hangman.py
@@ -5,14 +5,16 @@ words = ["apple", "banana", "mango", "strawberry", "orange", "grape", "pineapple
 word = random.choice(words).upper()
 print("Welcome to the Hangman game.")        
 
+lives = 5
 
 def hangman():
+    global lives
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     
-    while len(word_letters) > 0:
-        print("You have used these letters",' '.join(used_letters))
+    while len(word_letters) > 0 and lives > 0:
+        print(f"You have {lives} left. You have used these letters",' '.join(used_letters))
         
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word:',' '.join(word_list))
@@ -21,14 +23,21 @@ def hangman():
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
-                word_letters.remove(user_letter)
+                word_letters.remove(user_letter) # not for storing but tracking, once all letters are removed from word_letters then it means the word has been fully guessed.
+                
+            else:
+                lives = lives - 1
+                print("Letter is not in word.")
 
         elif user_letter in used_letters:
             print("You have already used that character. Please try again.")
 
         else:
             print("Invalid character.")
-    print("You guessed the word", word)
+    if lives == 0:
+        print("Sry.The Hangman is dead.")
+    else:
+        print("You guessed the word", word)
 
 hangman()
 
